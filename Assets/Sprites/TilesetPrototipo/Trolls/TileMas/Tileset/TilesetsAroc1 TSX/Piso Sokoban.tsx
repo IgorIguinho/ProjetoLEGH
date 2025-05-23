6c531ddabafb9792a7544508455e708bebbee055
IgorIguinho <igor.rohrig@prototipandoaquebrada.org>
2025-05-23 19:50:29 -0300

    NPCs que com animação ato 1

--- Assets/Sprites/TilesetPrototipo/Trolls/TileMas/Tileset/TilesetsAroc1 TSX/Piso Sokoban.tsx
+++ Assets/Sprites/TilesetPrototipo/Trolls/TileMas/Tileset/TilesetsAroc1 TSX/Piso Sokoban.tsx
@@ -72,9 +72,4 @@
    <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
- <tile id="60">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="32" height="32"/>
-  </objectgroup>
- </tile>
 </tileset>
